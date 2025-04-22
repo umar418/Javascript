27af721728ecfb9ca1e0a496e8001b1b11352fb1
Muhammad Umar Farooq <129897669+umar418@users.noreply.github.com>
2025-04-22 11:22:10 +0000

    varibales

---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+const accountId=1213322
+let accountEmail='javascript@gmail.com'
+var accountPassword="4wwefd"
+let accountCity="Islamabad"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+console.table(typeof accountId)
